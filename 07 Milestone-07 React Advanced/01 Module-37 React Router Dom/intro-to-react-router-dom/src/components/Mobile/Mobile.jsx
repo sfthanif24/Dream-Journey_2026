bf8fa309b67bbd57984e75232a5229f87a1c4f72
Hanif <mohammadabuhanif95@gmail.com>
2026-04-03 20:04:27 +0800

    Update React Router Dom main entry and add initial components

---
+++ 07 Milestone-07 React Advanced/01 Module-37 React Router Dom/intro-to-react-router-dom/src/components/Mobile/Mobile.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Mobile = () => {
+  return <div>This is Mobile Page</div>;
+};
+
+export default Mobile;
--- 07 Milestone-07 React Advanced/01 Module-37 React Router Dom/intro-to-react-router-dom/src/main.jsx
+++ 07 Milestone-07 React Advanced/01 Module-37 React Router Dom/intro-to-react-router-dom/src/main.jsx
@@ -1,19 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from "./components/Root/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import Mobile from "./components/Mobile/Mobile.jsx";
+import Laptop from "./components/Laptop/Laptop.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Hello From React Router</div>
-  }
-])
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "mobile", Component: Mobile },
+      { path: "laptop", Component: Laptop },
+    ],
+  },
+  {
+    path: "about",
+    element: <div>About me Here</div>,
+  },
+  {
+    path: "blogs",
+    element: <div>All my blogs are here</div>,
+  },
+  {
+    path: "app",
+    Component: App,
+  },
+  {
+    path: "/app2",
+    // element: <App></App>,
+    Component: App,
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
